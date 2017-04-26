c894355f72856485c2534e3f5b98cab9c1739611
r4ph431 <raphaelbezd12@gmail.com>
2017-04-26 22:42:11 +0200

    added M-UI Buttons

--- src/components/protected/Main.js
+++ src/components/protected/Main.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import Button from 'muicss/lib/react/button'
+import RaisedButton from 'material-ui/RaisedButton'
 
 export default class main extends Component {
   render () {
     return (
       <div>
         Main. This is a protected route. You can only see this if you're authed.
-        <Link to="/create"><Button variant="raised" color="primary">Create</Button></Link>
-        <Link to="/join"><Button variant="raised" color="primary">Join</Button></Link>
+        <Link to="/create"><RaisedButton label="Create" primary={true} /></Link>
+        <Link to="/join"><RaisedButton label="Join" primary={true} /></Link>
       </div>
     )
   }
