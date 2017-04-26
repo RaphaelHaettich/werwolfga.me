29826a0b695b671cb244371d93dad9949568fdd2
Hättich Raphael <Raphael.Haettich@swisscom.com>
2017-04-26 17:44:32 +0200

    cancel button component

---
+++ src/components/protected/Cancelbutton.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+import Button from 'muicss/lib/react/button'
+import {base} from '../../config/constants'
+import { withRouter } from 'react-router-dom'
+
+class cancelbutton extends Component {
+
+  delete() {
+    base
+      .remove('activegame/' + this.props.lobbyKey)
+      .then(() => {
+        console.log(this.props)
+        this.props.history.push('/main')
+      })
+      .catch(error => {
+        //handle error
+      });
+  }
+  
+  render() {
+    return (
+      <Button onClick={this
+        .delete
+        .bind(this)} variant="raised">
+        Cancel
+      </Button>
+    )
+  }
+}
+
+export default withRouter(cancelbutton)
