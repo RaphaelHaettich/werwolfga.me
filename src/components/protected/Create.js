78aea75655cde3dc6911979379badee72ce48c37
Hättich Raphael <Raphael.Haettich@swisscom.com>
2017-04-26 16:32:13 +0200

    create lobby firebase

--- src/components/protected/Create.js
+++ src/components/protected/Create.js
@@ -1,16 +1,57 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Cards from './Cards'
-import Joinedcount from './Joinedcount'
+import Joinedcounter from './Joinedcounter'
+import { post } from '../../helpers/dbcalls'
+import { base } from '../../config/constants'
+
 
 class create extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      lobbyId: "",
+      lobbyKey: "",
+      loading: true
+    };
+  }
+  componentDidMount() {
+    var self = this;
+
+    let inviteCode = Math.floor(Math.random() * 900000) + 100000
+    const userId = base.app().INTERNAL.getUid()
+    let collection = 'activegame'
+
+    let createLobby = new Promise((resolve, reject) => {
+      post(resolve, reject, inviteCode, userId, collection);
+    })
+    createLobby.then((key) => {
+      self.setState({
+        lobbyId: inviteCode,
+        lobbyKey: key,
+        loading: false
+      });
+    }).catch(function (error) {
+      alert("Error: " + error);
+    });
+  }
+
   render() {
     return (
       <div>
-        <h3>
-          Available Cards
+        {this.state.loading === true
+          ? <h3>
+            LOADING...
+            </h3>
+          :
+          <div>
+            <h3>
+              Available Cards
         </h3>
-        <Cards /> 
-        <Joinedcount />
+            <Cards />
+            <h3>Lobby ID: {this.state.lobbyId}</h3>
+            <Joinedcounter lobbyKey={this.state.lobbyKey}/>
+          </div>
+        }
       </div>
     )
   }
---
+++ src/helpers/dbcalls.js
@@ -0,0 +1,18 @@
+import { base } from '../config/constants'
+
+
+export var post = (resolve, reject, inviteCode, userId, collection) => {
+    base.push(collection, {
+        data: {code: inviteCode, host: userId, state: 'draft'}
+    }).then(snapshot => {
+        console.log(snapshot)
+        resolve(snapshot.key)
+    }).catch(err => {
+        //handle error
+        alert("post failed "+ err)
+    });
+}
+
+
+
+
