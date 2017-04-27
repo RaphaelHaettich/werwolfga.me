6328a0129f30fd0348cb9519c04cd18ebb47d3a2
Hättich Raphael <Raphael.Haettich@swisscom.com>
2017-04-27 17:23:33 +0200

    began work join

--- src/pages/Join/Join.js
+++ src/pages/Join/Join.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
+import {Card, CardActions, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import TextField from 'material-ui/TextField';
+import { Container, Row, Col, Visible, Hidden, ScreenClassRender } from 'react-grid-system';
 
 export default class join extends Component {
   render () {
     return (
-      <div>
-        Join. This is a protected route. You can only see this if you're authed.
-      </div>
+      <Card>
+        <CardTitle title="Join Game" />
+        <CardActions>
+        <Container>
+        <Row>
+        <Col xs={6}>
+          <TextField
+            hintText="Hint Text"
+            floatingLabelText="Floating Label Text"
+          />
+          </Col>
+          <Col xs={6}>
+          <FlatButton label="Action2" />
+          </Col>
+          </Row>
+          </Container>
+        </CardActions>
+        <CardText >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </CardText>
+      </Card>
     )
   }
 }
