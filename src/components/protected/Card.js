2108f7bf79ff829e13bcc09430a3a74d38000c84
Raphael H <Raphael H>
2017-04-27 09:00:28 +0200

    linted

--- src/components/protected/Card.js
+++ src/components/protected/Card.js
@@ -5,23 +5,21 @@ import Divider from 'material-ui/Divider'
 import {
   Card,
   CardActions,
-  CardHeader,
   CardText,
   CardMedia,
   CardTitle
-} from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
+} from 'material-ui/Card'
 
 class card extends Component {
   render() {
     var styles = {
       cardImage: {
-        maxHeight: "200px", 
-        display: "block", 
-        marginLeft: "auto", 
-        marginRight: "auto", 
+        maxHeight: "200px",
+        display: "block",
+        marginLeft: "auto",
+        marginRight: "auto",
         width: "auto",
-        minWidth: "0%"  
+        minWidth: "0%"
       },
       card: {
         marginBottom: "15px"
@@ -30,18 +28,22 @@ class card extends Component {
     let item = this.props.item
     return (
       <div style={styles.card}>
-        <Divider />
+        <Divider/>
         <Card>
-          <CardMedia
-            overlay={< CardTitle title = {item.name} />}>
-            <img style={styles.cardImage} src={item.picturefront} />
+          <CardMedia overlay={< CardTitle title={
+            item.name
+          } />}>
+            <img style={styles.cardImage} src={item.picturefront} alt="cardimage"/>
           </CardMedia>
-          <CardTitle subtitle="Expand for Description" actAsExpander={true} showExpandableButton={true}/>
+          <CardTitle
+            subtitle="Expand for Description"
+            actAsExpander={true}
+            showExpandableButton={true}/>
           <CardText expandable={true}>
             {item.description}
           </CardText>
           <CardActions>
-            <CountButton />
+            <CountButton/>
           </CardActions>
         </Card>
       </div>
