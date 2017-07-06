7325318c7bc26478655ea2fb82f58066df4337d0
Raphael H <Raphael H>
2017-07-06 19:54:50 +0200

    hide name and desc if hidden card

--- src/components/Flipcard/Flipcard.js
+++ src/components/Flipcard/Flipcard.js
@@ -4,8 +4,8 @@
 */
 
 import React, {Component} from 'react'
-import {Card, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import Paper from 'material-ui/Paper';
+import Styles from './Flipcard.css.js'
 import ReactCardFlip from 'react-card-flip';
 import SimpleState from 'react-simple-state'
 const simpleState = new SimpleState()
@@ -37,14 +37,21 @@ class card extends Component {
 
         return (
             <div >
+                {this.state.isFlipped === false ?
                 <Paper zDepth={2}>
                     <h1>{item.name}</h1>
-                    <h3>{item.description}</h3>
+                    <h3>{item.description}</h3> 
                 </Paper>
+                :
+                <Paper zDepth={2}>
+                    <h1>Hidden</h1>
+                    <h3>Hidden</h3> 
+                </Paper>
+                    }
                 <ReactCardFlip isFlipped={this.state.isFlipped}>
-                    <img onClick={this.handleClick} key="front" src={item.picturefront} alt=""/>
+                    <img style={Styles.cardImage} onClick={this.handleClick} key="front" src={item.picturefront} alt=""/>
 
-                    <img onClick={this.handleClick} key="back" src={item.pictureback} alt=""/>
+                    <img style={Styles.cardImage} onClick={this.handleClick} key="back" src={item.pictureback} alt=""/>
                 </ReactCardFlip>
             </div>
         )
