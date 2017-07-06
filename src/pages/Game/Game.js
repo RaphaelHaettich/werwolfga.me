2a9b0f92d78fd5d8bfcb2cf4fb4150847badfb14
Hättich Raphael <Raphael.Haettich@swisscom.com>
2017-07-06 16:00:02 +0200

    game logic start

--- src/pages/Game/Game.js
+++ src/pages/Game/Game.js
@@ -1,10 +1,42 @@
 import React, {Component} from 'react'
+import {fetch} from '../../helpers/dbcalls'
+import {base} from '../../config/constants'
 import SimpleState from 'react-simple-state'
 const simpleState = new SimpleState()
 
 export default class Gameadmin extends Component {
   componentDidMount(){
-    simpleState.evoke("loader", false)
+    const userId = base
+      .app()
+      .INTERNAL
+      .getUid()
+    console.log(simpleState.getState("gameId"))
+    if(simpleState.getState("gameId").id === ""){
+      this.props.history.push("join")
+    }else{
+
+      let getCurrentCard = new Promise((resolve, reject) => {
+        console.log(simpleState.getState("gameId").id)
+        const collection = 'activegame/' + simpleState.getState("gameId").id + "/memberarray/" + userId
+        fetch(resolve, reject, collection);
+      })
+      getCurrentCard.then((data) => {
+        console.log("to route")
+        console.log(data)
+        let getCardInfos = new Promise((resolve, reject) => {
+          console.log(simpleState.getState("gameId").id)
+          const collection = 'cards/' + data[0]
+          fetch(resolve, reject, collection);
+        })
+        getCardInfos.then((data) => {
+          console.log("got card")
+          console.log(data)
+          simpleState.evoke("loader", false)
+        })
+      })
+
+    }
+    
   }
   render() {
     return (
