ed73d4276c59daacb47b99f3bc4a572f780390d1
Raphael H <Raphael H>
2017-07-06 19:54:33 +0200

    flipcard CSS

---
+++ src/components/Flipcard/Flipcard.css.js
@@ -0,0 +1,13 @@
+var styles = {
+      cardImage: {
+        height: "300px",
+        maxHeight: "100%",
+        display: "block",
+        marginLeft: "auto",
+        marginRight: "auto",
+        width: "auto",
+        minWidth: "0%"
+      }
+    };
+
+export default styles;
