1926e6de09f58c9bdc90619052bccd8c00d21c61
Hättich Raphael <Raphael.Haettich@swisscom.com>
2017-07-06 17:42:49 +0200

    created flipcard

--- src/components/Flipcard/Flipcard.js
+++ src/components/Flipcard/Flipcard.js
@@ -5,39 +5,50 @@
 
 import React, {Component} from 'react'
 import {Card, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import Paper from 'material-ui/Paper';
+import ReactCardFlip from 'react-card-flip';
 import SimpleState from 'react-simple-state'
 const simpleState = new SimpleState()
 
-
-
 class card extends Component {
 
-  componentDidMount() {
-    simpleState.evoke("loader", false)
-    console.log(simpleState.getState("loader"))
-  }
+    componentDidMount() {
+        simpleState.evoke("loader", false)
+        console.log(simpleState.getState("loader"))
+    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            isFlipped: false
+        };
+        this.handleClick = this
+            .handleClick
+            .bind(this);
+    }
+    handleClick(e) {
+        e.preventDefault();
+        this.setState({
+            isFlipped: !this.state.isFlipped
+        });
+    }
 
+    render() {
+        let item = this.props.data
 
-  render() {
-    let item = this.props.data
+        return (
+            <div >
+                <Paper zDepth={2}>
+                    <h1>{item.name}</h1>
+                    <h3>{item.description}</h3>
+                </Paper>
+                <ReactCardFlip isFlipped={this.state.isFlipped}>
+                    <img onClick={this.handleClick} key="front" src={item.picturefront} alt=""/>
 
-    return (
-      <div >
-        <Card>
-            <CardMedia>
-            <img src={item.picturefront} alt="" />
-            </CardMedia>
-            <CardTitle title="Card title"/>
-            <CardText>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-            </CardText>
-        </Card>
-      </div>
-    )
-  }
+                    <img onClick={this.handleClick} key="back" src={item.pictureback} alt=""/>
+                </ReactCardFlip>
+            </div>
+        )
+    }
 }
 
 export default card
