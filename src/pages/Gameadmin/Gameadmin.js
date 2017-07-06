2d495ae1e0d8f3c6793a7e8682e433a375c9e3b7
Raphael H <Raphael H>
2017-07-06 09:14:41 +0200

    cleaned up

--- src/pages/Gameadmin/Gameadmin.js
+++ src/pages/Gameadmin/Gameadmin.js
@@ -1,19 +1,10 @@
 import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
-import RaisedButton from 'material-ui/RaisedButton'
 
-export default class Home extends Component {
+export default class Gameadmin extends Component {
   render() {
     return (
       <div className="col-sm-6 col-sm-offset-3">
-        Home. Not Protected. Anyone can see this.
-        <br/>
-        <Link to="/login">
-          <RaisedButton label="Login" primary={true}/>
-        </Link>
-        <Link to="/register">
-          <RaisedButton label="Registration" primary={true} />
-        </Link>
+        Gameadmin
       </div>
     )
   }
