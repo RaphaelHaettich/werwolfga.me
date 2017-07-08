2d4996b5832de43c33cbaf8d5bed908fdc7c13ec
Raphael <raphaelbezd12@gmail.com>
2017-07-08 23:32:04 +0200

    next step gameadmin

--- src/pages/Gameadmin/Gameadmin.js
+++ src/pages/Gameadmin/Gameadmin.js
@@ -9,7 +9,7 @@ export default class Gameadmin extends Component {
   componentDidMount(){
     simpleState.evoke("loader", false)
     if(simpleState.getState("gameId").id === "" && cookies.get('lobbyNumber') === undefined){
-      this.props.history.push("join")
+      this.props.history.push("main")
     }else{
       let gameId = "";
       if(simpleState.getState("gameId").id !== ""){
@@ -19,32 +19,39 @@ export default class Gameadmin extends Component {
         console.log(cookies.get('lobbyNumber'))
         gameId = cookies.get('lobbyNumber')
       }
-      let getCards = new Promise((resolve, reject) => {
+      let getActiveCards = new Promise((resolve, reject) => {
         console.log(gameId)
         const collection = 'activegame/' + gameId + "/memberarray/"
         console.log(collection)
         fetch(resolve, reject, collection);
       })
-      getCards.then((data) => {
+      getActiveCards.then((data) => {
         console.log("derp")
         console.log(data)
-        // let getCardInfos = new Promise((resolve, reject) => {
-        //   console.log(simpleState.getState("gameId").id)
-        //   const collection = 'cards/' + data[0]
-        //   fetch(resolve, reject, collection);
-        // })
-        // getCardInfos.then((data) => {
-        //   console.log("got card")
-        //   console.log(data)
-        //   let cardObj = {
-        //     description: data[0],
-        //     name: data[1],
-        //     pictureback: data[2],
-        //     picturefront: data[3]
-        //   }
-        //   this.setState({cards: cardObj})
-        //   simpleState.evoke("loader", false)
-        // })
+        const activeData = data;
+
+
+        let getCardInfos = new Promise((resolve, reject) => {
+          console.log(simpleState.getState("gameId").id)
+          const collection = 'cards/'
+          fetch(resolve, reject, collection);
+        })
+        getCardInfos.then((data) => {
+          console.log("got card")
+          console.log(data)
+          console.log(activeData.length)
+          for(var i = 0; i < activeData.length; i++){
+            console.log(activeData[i])
+          }
+          // let cardObj = {
+          //   description: data[0],
+          //   name: data[1],
+          //   pictureback: data[2],
+          //   picturefront: data[3]
+          // }
+          // this.seteState({cards: cardObj})
+          // simpleState.evoke("loader", false)
+        })
       })
     }
   }
