83c6b224f59a7901e3251986469e9cc2ca70613a
Raphael <raphaelbezd12@gmail.com>
2017-07-15 00:03:18 +0200

    storage bucket for future use

--- src/config/constants.js
+++ src/config/constants.js
@@ -4,7 +4,8 @@ import Rebase from 're-base';
 var connection = Rebase.createClass({
     apiKey: "AIzaSyA4imvHroiFIbJsjhdmCgBXSLBa0LAQ3bs",
     authDomain: "werewolvesgame-e65c4.firebaseapp.com",
-    databaseURL: "https://werewolvesgame-e65c4.firebaseio.com"
+    databaseURL: "https://werewolvesgame-e65c4.firebaseio.com",
+    storageBucket: "werewolvesgame-e65c4.appspot.com"
 });
 
 
