8df3f19337b4682857dfb4ab54ce9ab529fd53a0
Raphael <raphaelbezd12@gmail.com>
2017-07-14 13:15:14 +0200

    added gamadmin css with ability to center

---
+++ src/pages/Gameadmin/Gameadmin.css.js
@@ -0,0 +1,11 @@
+var styles = {
+      centeredOnlyHorizontal: {
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        marginTop: "50px",
+        marginBottom: "10%",
+        position: "absolute"
+      }
+    };
+
+export default styles;
