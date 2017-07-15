ac492a869d342051b4285eb2650836e5d2252c52
Raphael <raphaelbezd12@gmail.com>
2017-07-15 18:21:28 +0200

    added css

--- src/components/Flipcard/Flipcard.css.js
+++ src/components/Flipcard/Flipcard.css.js
@@ -2,11 +2,17 @@ var styles = {
       cardImage: {
         height: "300px",
         maxHeight: "100%",
+        maxWidth: "300px",
         display: "block",
         marginLeft: "auto",
         marginRight: "auto",
         width: "auto",
-        minWidth: "0%"
+        minWidth: "0%",
+        backgroundColor: "dimgray",
+        padding: "14px"
+      },
+      paper: {
+        marginBottom: "50px"
       }
     };
 
