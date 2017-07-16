bdabac863712474abe1910dd199f44a4cca5d171
Raphael <raphaelbezd12@gmail.com>
2017-07-16 11:06:52 +0200

    send info to user when game finsihed

--- src/pages/Game/Game.js
+++ src/pages/Game/Game.js
@@ -3,6 +3,7 @@ import {fetch} from '../../helpers/dbcalls'
 import {base} from '../../config/constants'
 import Flipcard from '../../components/Flipcard/Flipcard'
 import SimpleState from 'react-simple-state'
+import Warningwindow from '../../components/Warningwindow/Warningwindow'
 const simpleState = new SimpleState()
 
 
@@ -14,6 +15,13 @@ export default class Gameadmin extends Component {
       cards: [],
       displayName: null
     }
+  }
+
+  gameDone = () => {
+    this
+      .props
+      .history
+      .push("main")
   }
 
   componentDidMount(){
@@ -36,31 +44,57 @@ export default class Gameadmin extends Component {
         fetch(resolve, reject, collection);
       })
       getCurrentCard.then((data) => {
-        this.setState({displayName: data[1]})
-        let getCardInfos = new Promise((resolve, reject) => {
-          const collection = 'cards/' + data[0]
-          fetch(resolve, reject, collection);
-        })
-        getCardInfos.then((data) => {
-          let cardObj = {
-            description: data[0],
-            name: data[1],
-            pictureback: data[2],
-            picturefront: data[3]
-          }
-          this.setState({cards: cardObj})
-          simpleState.evoke("loader", false)
-        })
+        console.log(data)
+        if(data.length > 0){
+          this.setState({displayName: data[1]})
+          let getCardInfos = new Promise((resolve, reject) => {
+            const collection = 'cards/' + data[0]
+            fetch(resolve, reject, collection);
+          })
+          getCardInfos.then((data) => {
+            let cardObj = {
+              description: data[0],
+              name: data[1],
+              pictureback: data[2],
+              picturefront: data[3]
+            }
+            this.setState({cards: cardObj})
+            simpleState.evoke("loader", false)
+            base.listenTo('activegame/' + gameId + '/state', {
+              context: this,
+              asArray: false,
+              then(data){
+                console.log(data)
+                if(data !== "ready"){
+                  this
+                  .dialog
+                  .handleOpen()  
+                }
+              }
+            })
+          })
+        }else{
+          this
+          .props
+          .history
+          .push("join")
+        }
       })
-
     }
-    
   }
   render() {
     return (
       <div className="col-sm-6 col-sm-offset-3">
       <h2>{this.state.displayName} your card is:</h2>
-        <Flipcard data={this.state.cards}/>
+      <Flipcard data={this.state.cards}/>
+      <Warningwindow
+        message={"Game finished"}
+          secondAction={this.gameDone}
+          secondActionShow={true}
+          secondActionLabel={"Leave game"}
+          ref={(dialog) => {
+          this.dialog = dialog
+        }}/>
       </div>
     )
   }
