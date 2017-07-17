30a63cb1e82809d73ce475dbfb7000ccb0411a8b
Raphael <raphaelbezd12@gmail.com>
2017-07-18 01:00:15 +0200

    added game.css

---
+++ src/pages/Game/Game.css.js
@@ -0,0 +1,19 @@
+var styles = {
+      centeredOnlyHorizontal: {
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        marginTop: "50px",
+        marginBottom: "10%",
+        position: "absolute"
+      },
+      fab: {
+        margin: 0,
+        top: 'auto',
+        right: 20,
+        bottom: 20,
+        left: 'auto',
+        position: 'fixed',
+      }
+    };
+
+export default styles;
