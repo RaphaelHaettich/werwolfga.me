68caaaf8864f2a3dd10edfb08c0f9172b2feed27
Raphael <raphaelbezd12@gmail.com>
2017-07-18 10:38:56 +0200

    added checkbox checked state

--- src/components/Checkboxlist/Checkboxlist.js
+++ src/components/Checkboxlist/Checkboxlist.js
@@ -7,11 +7,13 @@ class CheckboxList extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      votedkey: ""
+      votedkey: "",
+      checkboxChecked: false
     };
   }
   handleChange(event) {
     this.setState({votedkey: event.target.value});
+    this.setState({checkboxChecked: true});
     console.log(event.target);
   }
 
