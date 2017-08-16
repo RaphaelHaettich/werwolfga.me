84263e236ada2142b263e463d6edc6917fecf1ed
Raphael Hättich <raphaelbezd12@gmail.com>
2017-08-16 13:55:42 +0200

    sharebutton functions

--- src/components/ShareFAB/index.js
+++ src/components/ShareFAB/index.js
@@ -1,12 +1,6 @@
 /*
 *   Required Props:
-*   key: string,
-*   item: object
-*   counter: boolean   
-*   actionCard: boolean
-*
-*   Required simpleState:
-*   cards
+*   lobbyKey: string,
 * 
 */
 
@@ -18,16 +12,16 @@ import Mail from 'material-ui/svg-icons/content/mail';
 import Copy from 'material-ui/svg-icons/content/content-copy';
 import Styles from './style.css.js';
 import WhatsApp from '../SvgIcons/WhatsApp';
-// import FloatingActionButton from 'material-ui/FloatingActionButton';
-// import Delete from 'material-ui/svg-icons/action/delete';
-
-
+import clipboard from '../../helpers/clipboard';
+import Snackbar from 'material-ui/Snackbar';
 
 class ChildCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
       count: 0,
+      open: false,
+      snackMessage: '',
     };
   }
 
@@ -35,40 +29,66 @@ class ChildCard extends Component {
 
   }
 
-  fabAction1 = () => {
-    // this.props.action(this.props.item.userKey);
+  fabActionMail = () => {
+    // eslint-disable-next-line max-len
+    const body = `Join%20the%20game%20with%20this%20Link:%20${location.origin}/invitelink?id=${this.props.lobbyKey}.`;
+    window.location.href = `mailto:?subject=Werwolf%20Game%20Invite&body=${body}`;
+  };
+  fabActionWhatsApp = () => {
+    // eslint-disable-next-line max-len
+    const body = `You%20are%20invited%20to%20join%20the%20Werwolfgame%20with%20this%20Link:%20${location.origin}/invitelink?id=${this.props.lobbyKey}.`;
+    window.location.href = `whatsapp://send?text=${body}`;
+  };
+  fabActionCopy = () => {
+    // eslint-disable-next-line max-len
+    const copyToClipboard = clipboard(`${location.origin}/invitelink?id=${this.props.lobbyKey}`);
+    console.log(copyToClipboard);
+    if (copyToClipboard) {
+      this.setState({ open: true, snackMessage: 'Successful copied link to clipboard', });
+    } else {
+      this.setState({ open: true, snackMessage: 'Copy link to clipboard failed', });
+    }
   };
 
   render() {
     return (
-      <SpeedDial
-        style={Styles.speedDial}
-        fabContentOpen={
-          <Share />
-        }
-        fabContentClose={
-          <NavigationClose />
-        }
-      >
-        <SpeedDialItem
-          label="Whatsapp"
-          fabContent={<WhatsApp />}
-          onTouchTap={this.fabAction1}
-          style={Styles.speedDialItem}
+      <div>
+        <SpeedDial
+          style={Styles.speedDial}
+          fabContentOpen={
+            <Share />
+          }
+          fabContentClose={
+            <NavigationClose />
+          }
+        >
+          <SpeedDialItem
+            label="Whatsapp"
+            fabContent={<WhatsApp Style={{ fill: 'green', }} />}
+            onTouchTap={this.fabActionWhatsApp}
+            style={Styles.speedDialItem}
+          />
+          <SpeedDialItem
+            label="Email"
+            fabContent={<Mail style={{ fill: 'white', }} />}
+            onTouchTap={this.fabActionMail}
+            style={Styles.speedDialItem}
+          />
+          <SpeedDialItem
+            label="Copy to clipboard"
+            fabContent={<Copy style={{ fill: 'white', }} />}
+            onTouchTap={this.fabActionCopy}
+            style={Styles.speedDialItem}
+          />
+        </SpeedDial>
+        <Snackbar
+          open={this.state.open}
+          message={this.state.snackMessage}
+          autoHideDuration={4000}
+          bodyStyle={{ backgroundColor: 'rgb(48, 48, 48)', }}
+          contentStyle={{ color: 'white', }}
         />
-        <SpeedDialItem
-          label="Email"
-          fabContent={<Mail />}
-          onTouchTap={this.fabAction1}
-          style={Styles.speedDialItem}
-        />
-        <SpeedDialItem
-          label="Copy to clipboard"
-          fabContent={<Copy />}
-          onTouchTap={this.fabAction1}
-          style={Styles.speedDialItem}
-        />
-      </SpeedDial>
+      </div>
     );
   }
 }
