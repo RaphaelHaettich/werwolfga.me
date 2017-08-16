744723da71e88a1ee69522d3723d52aa67e8daf1
Raphael Hättich <raphaelbezd12@gmail.com>
2017-08-16 13:56:36 +0200

    added lobbykey prop to sharebutton

--- src/pages/Create/index.js
+++ src/pages/Create/index.js
@@ -229,7 +229,7 @@ class Create extends Component {
               </div>}
           </div>
         </div>
-        <ShareFAB />
+        <ShareFAB lobbyKey={this.state.lobbyId} />
       </div>
     );
   }
